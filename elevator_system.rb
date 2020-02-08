efd4f96a42bc9ce685a5700cc9d3090c6bab0c2f
Spencer Miller <dogpantz@gmail.com>
2020-02-07 17:15:42 -0700

    working on elevator system logic, realizing it needs a lot more work :)

--- elevator_system.rb
+++ elevator_system.rb
@@ -23,24 +23,39 @@ class ElevatorSystem
     @requests << {
       floor: floor,
       direction: direction
-    } unless elevator_requested?
+    } unless elevator_requested?(floor: floor, direction: direction)
   end
 
   def ready_for_floor_requests?
-    !@open_elevator.nil?
+    !open_elevator.nil?
   end
 
   def floor_request(floor)
+    unless ready_for_floor_requests?
+      puts "Please wait for passengers to be picked up before requesting a floor"
+    end
+
+    open_elevator.add_passenger(floor)
   end
 
   def time_passed
-    elevators.next do |elevator|
-      elevator.move
-      elevator.release_passengers
-    end
+    move_elevator elevators.next
   end
 
   private
+
+  def move_elevator(elevator)
+    elevator.move
+    dropped_off = elevator.release_passengers
+    puts "Passenger(s) dropped off on floor #{elevator.floor + 1}" if dropped_off
+
+    if elevator_requested?(floor: elevator.floor, direction: elevator.direction)
+      @requests.delete({ floor: elevator.floor, direction: elevator.direction })
+      puts "Passenger(s) picked up from floor #{elevator.floor + 1}"
+    end
+
+    elevator
+  end
 
   def elevator_requested?(floor:, direction:)
     @requests.include?({ floor: floor, direction: direction })
